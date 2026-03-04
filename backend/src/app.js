84b3ab2f3f95e1601d8aea2d3d73a82b668c74dc
jainshab1803 <abhijain1803g@gmail.com>
2026-03-04 17:32:42 +0530

    prepare for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,10 @@ import morgan from "morgan"
 import routes from "../src/routes/index.js"
 const app = express();
 
+
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+    "http://localhost:3000",
     "https://consistently.vercel.app"
   ],
   credentials: true
--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../app/store";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true
 });
 
