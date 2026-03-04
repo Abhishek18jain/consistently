3833c0f156089a40ea71a3ea69136827886d81ed
jainshab1803 <abhijain1803g@gmail.com>
2026-03-04 17:07:49 +0530

    added backend routes

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../app/store";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://consistently-15d8.onrender.com || http://localhost:5000/api"
 });
 
 api.interceptors.request.use((config) => {
